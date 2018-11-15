b15e637608e390165556174da2ff27f4a2ac8130
kolla47 <akhil.trinayan@gmail.com>
2018-11-15 19:04:33 +0530

    Web-craweler

---
+++ download.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const uuid = require('uuid/v1')
+
+const downloadPage = (url='http://nodeprogram.com') => {
+  console.log('downloading ', url)
+  const fetchPage = (urlF, callback) => {
+    http.get(urlF, (response) => {
+      let buff = ''
+      response.on('data', (chunk) => {
+        buff += chunk
+      })
+      response.on('end', () => {
+        callback(null, buff)
+      })
+    }).on('error', (error) => {
+      console.error(`Got error: ${error.message}`)
+      callback(error)
+    })
+  }
+  const folderName = uuid()
+  fs.mkdirSync(folderName)
+  fetchPage(url, (error,data) =>{
+    if (error) return console.log(error)
+    fs.writeFileSync(path.join(__dirname, folderName, 'url.txt'), url)
+    fs.writeFileSync(path.join(__dirname, folderName, 'file.html'),data)
+    console.log('Downloading is done in folder ',folderName)
+  })
+}
+
+downloadPage(process.argv[2])
